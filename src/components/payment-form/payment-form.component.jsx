af719abe019556ae849775ef8365212d08b0a975
Phenk0 <roman.s.parkhomenko@gmail.com>
2022-10-22 09:16:32 +0300

    continue to implement stripe-payment system (vid#186)

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+
+import { selectCartTotal } from "../../store/cart/cart.selector";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
@@ -7,11 +12,15 @@ import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const amount = useSelector(selectCartTotal);
+  const currentUser = useSelector(selectCurrentUser);
 
   const paymentHandler = async (e) => {
     e.preventDefault();
+
     if (!stripe || !elements) return;
-    const response = await fetch("/netlify/functions/create-payment-intent", {
+
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
       method: "post",
       headers: {
         "Content-Type": "application/json",
@@ -19,7 +28,28 @@ const PaymentForm = () => {
       body: JSON.stringify({ amount: 10000 }),
     }).then((res) => res.json());
 
-    console.log(response);
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Roman Parkhomenko",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Successful");
+      }
+    }
   };
   return (
     <PaymentFormContainer>
